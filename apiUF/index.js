9076fe772d2aabef15c42d247acacfcc79ea175d
MaxCostaCardosoIFRO <maxxxuelconta69@gmail.com>
2024-09-26 11:40:41 -0400

    oi

--- apiUF/index.js
+++ apiUF/index.js
@@ -1,15 +1,44 @@
-const express = require ('express');
-const colecaoUf = require ('./dados/dados.js');
+const express = require('express');
+const colecaoUf = require('./dados/dados.js');
 
 const app = express();
 
-app.get('/ufs',(req, res) =>{
+app.get('/ufs', (req, res) => {
 
-res.json(colecaoUf.colecaoUf)
+    res.json(colecaoUf.colecaoUf)
 
 
+})
+
+app.get('/ufs/:iduf', (req, res) => {
+
+    const idUF = parseInt(req.params.iduf)
+    const uf = colecaoUf.colecaoUf.find(u => u.id === idUF)
+    res.json(uf)
+
+    let mensagemerro = '';
+
+    if (!(isNaN(idUF))) {
+        uf = colecaoUf.colecaoUf.find(u => u.id === idUF)
+        if (!uf) {
+            mensagemerro = "uf não encontrada"
+        }
+        else {
+            mensagemerro = 'requisição invalida'
+        }
+    }
+    if (uf) {
+        res.json(uf);
+    } else {
+        res.status(404).json({ "erro": mensagemerro })
+    }
 });
-app.listen(8080,()=>{
-console.log("serviddor iniciado na porta 8080")
+
+
+
+
+
+app.listen(8080, () => {
+    console.log("serviddor iniciado na porta 8080")
 
 })
