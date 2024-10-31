f13d3b01a6ee08a5a738b7b7ebf86ef819a7e057
MaxCostaCardosoIFRO <maxxxuelconta69@gmail.com>
2024-10-31 10:19:51 -0400

    oi

--- apiUF/index.js
+++ apiUF/index.js
@@ -3,12 +3,22 @@ const colecaoUf = require('./dados/dados.js');
 
 const app = express();
 
+const buscarUfsPorNome = (nomeUf) => {
+    return colecaoUf.filter(uf => uf.nome.toLowerCase().includes(nomeUf.toLowerCase()));
+}
+
 app.get('/ufs', (req, res) => {
 
-    res.json(colecaoUf.colecaoUf)
+    const nomeut = req.query.busca;
+    const resultado = nomeUf ? buscarUfsPorNome(nomeUf) : colecaoUf;
+    if (resultado.length > 0) {
+        res.json(resultado);
+    } else {
+        res.status(404).send({ "erro": "Nenhuma UF encontrada" });
+    }
+});
 
 
-})
 
 app.get('/ufs/:iduf', (req, res) => {
 
